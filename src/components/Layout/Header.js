25bff1ec37b8913fd088a06df4ee0dcb77173972
TopeAanu <topefasasi99@gmail.com>
2024-10-14 21:41:45 +0100

    Day23: add gadget react app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import gadgetImage from "../../assets/comp.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Gadgets</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={gadgetImage} alt="Gadgets" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
